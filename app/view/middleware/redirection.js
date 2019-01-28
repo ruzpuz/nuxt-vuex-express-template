1e77fcec9c54565614157a148402ce3f18462c49
Vladimir Ruzic <vladimir@ruzic.info>
2019-01-28 20:15:06 +0100

    [#163221564] Simplified redirection middleware

--- app/view/middleware/redirection.js
+++ app/view/middleware/redirection.js
@@ -1,39 +1,34 @@
+function isAccessRoute(route) {
+  return (
+    route.name && (
+      route.name.startsWith('login') ||
+      route.name.startsWith('confirm-id') ||
+      route.name.startsWith('registration')
+    )
+  );
+}
+function isRouteSecure(route) {
+  return (
+    route.name && (
+      route.name.startsWith('dashboard')
+    )
+  );
+}
+
+
 export default async function ({ store, route, req, redirect, app }) {
   if (!process.server && !req) {
     return;
   }
-  function isAccessRoute(route) {
-    return (
-      route.name && (
-        route.name.startsWith('login') ||
-        route.name.startsWith('confirm-id') ||
-        route.name.startsWith('registration')
-      )
-    );
+
+  if(route.fullPath === '/') {
+    return redirect(app.localePath({ name: 'index' }));
   }
-  function isRouteSecure(route) {
-    return (
-      route.name && (
-        route.name.startsWith('dashboard')
-      )
-    );
+  if(isAccessRoute(route) && store.getters.isLoggedIn) {
+    return redirect(app.localePath({ name: 'dashboard' }));
   }
-
-  function resolveRoute() {
-
-    if(route.fullPath === '/') {
-      return redirect(app.localePath({ name: 'index' }));
-    }
-    if(isAccessRoute(route) && store.getters.isLoggedIn) {
-      return redirect(app.localePath({ name: 'dashboard' }));
-    }
-    if(isRouteSecure(route) && !store.getters.isLoggedIn) {
-      return redirect(app.localePath({ name: 'login' }));
-    }
-
+  if(isRouteSecure(route) && !store.getters.isLoggedIn) {
+    return redirect(app.localePath({ name: 'login' }));
   }
-
-  resolveRoute();
-
 
 }
