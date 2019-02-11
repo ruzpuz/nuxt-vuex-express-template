37d5e24d90bfe436bcf86a8d4f06c73eae805d54
Vladimir Ruzic <vladimir@ruzic.info>
2019-02-11 18:57:16 +0100

    [#163695252] Updated API documentation

--- app/api/authentication/restore-password/post/restore-password.route.js
+++ app/api/authentication/restore-password/post/restore-password.route.js
@@ -7,20 +7,23 @@ async function restorePasswordRoute(req, res) {
   res.status(response.code).json(response.payload);
 }
 /**
- * @api {post} /api/forgot-password 04 - Forgot password
- * @apiName Forgot password
+ * @api {post} /api/restore-password 05 - Restore password
+ * @apiName Restore password
  * @apiGroup Authentication
- * @apiDescription Api endpoint that starts forgot password procedure. Note that it will finish successfully even
+ * @apiDescription Api endpoint that finishes forgot password procedure. Note that it will finish successfully even
  * when there is no such email in the database
  *
  * @apiVersion 0.0.1
  *
- * @apiParam (Body) {String{128}} email Username
+ * @apiParam (Body) {String} restorationToken restorationToken
+ * @apiParam (Body) {String} password password
  *
- * @apiSuccess {String} FORGOT_PASSWORD_CALL_SUCCESSFUL A success message
+ * @apiSuccess {String} RESTORE_PASSWORD_SUCCESS A success message
  *
- * @apiError (Bad request 400) {String} FORGOT_PASSWORD_INVALID_DETAILS Happens when body does not contain email only
- * @apiError (Bad request 400) {String} FORGOT_PASSWORD_INVALID_EMAIL Happens when invalid email address is provided
+ * @apiError (Bad request 400) {String} RESTORE_PASSWORD_INVALID_DETAILS Happens when body does not contain necessary data only
+ * @apiError (Bad request 400) {String} RESTORE_PASSWORD_NO_PASSWORD Happens when password is not provided
+ * @apiError (Bad request 400) {String} RESTORE_PASSWORD_INVALID_RESTORATION_TOKEN Happens when invalid restoration token is provided
+ * @apiError (Not found 404) {String} RESTORE_PASSWORD_NO_USER_FOUND Happens when no user was found
  *
  * @apiError (Internal server error 500) {String} UNKNOWN_DATABASE_ERROR Happens when the operation failed for unknown reasons on the database. Should not happen in normal circumstances
  *
